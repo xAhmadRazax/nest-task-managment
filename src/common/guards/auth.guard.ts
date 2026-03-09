454f0cc5d904a06e72219400ebfb485710dbef45
Ahmad Raza <muhammad.ahmad.raza789@gmail.com>
2026-03-09 18:37:45 +0500

    chore: improve auth guard to handle edge case where user update password and token is still old one.

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -8,12 +8,14 @@ import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { JwtPayload } from '../interface/jwt-payload.interface';
 import { BlacklistTokenService } from 'src/auth/blacklist-token.service';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
     private readonly blackListTokenService: BlacklistTokenService,
+    private readonly userService: UsersService,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -29,6 +31,17 @@ export class AuthGuard implements CanActivate {
       // 💡 Here the JWT secret key that's used for verifying the payload
       // is the key that was passsed in the JwtModule
       const payload = await this.jwtService.verifyAsync<JwtPayload>(token);
+      const user = await this.userService.findOne(payload.email);
+
+      if (
+        !user ||
+        (user &&
+          payload.iat &&
+          user.passwordChangedAt.getTime() / 1000 > payload.iat)
+      ) {
+        await this.blackListTokenService.blacklistToken(token);
+        throw new UnauthorizedException();
+      }
       // 💡 We're assigning the payload to the request object here
       // so that we can access it in our route handlers
       request['user'] = payload;
