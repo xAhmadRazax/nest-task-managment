93625e9995b669e22118061f674e1ef328cf5737
Ahmad Raza <muhammad.ahmad.raza789@gmail.com>
2026-03-13 15:39:13 +0500

    chore: clean structure remove no longer need files

--- frontend-rr/src/features/auth/LoginForm.tsx
+++
@@ -1,9 +0,0 @@
-export const LoginForm = () => {
-  return (
-    <section className="max-w-[95%] mt-5  w-full mx-auto bg-red-50 ">
-      <h2>Welcome Back!</h2>
-
-      <form action=""></form>
-    </section>
-  );
-};
--- frontend-rr/src/pages/LoginPage.tsx
+++
@@ -1,8 +0,0 @@
-import { LoginForm } from "../features/auth/LoginForm";
-export const Login = () => {
-  return (
-    <div className="grid content-center min-h-screen w-screen bg-linear-to-br from-slate-950 to-olive-800  bg-slate-950">
-      <LoginForm />
-    </div>
-  );
-};
