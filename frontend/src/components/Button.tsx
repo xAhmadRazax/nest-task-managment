7232a756565d47ff861c2f498478f8696db40330
Ahmad Raza <muhammad.ahmad.raza789@gmail.com>
2026-03-17 03:19:13 +0500

    chore: create a Button component to make the link and button style consistent

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,43 @@
+import { Link } from '@tanstack/react-router'
+import type { LinkProps } from '@tanstack/react-router'
+import { Button as ShadCnButton } from '@/components/ui/Button'
+import { cn } from '#/lib/utils'
+
+export interface ButtonProps {
+  children: React.ReactNode
+  to?: LinkProps['to']
+  className?: string
+  type?: 'button' | 'link'
+}
+
+export const Button = ({
+  to,
+  type = 'link',
+  children,
+  className,
+}: ButtonProps) => {
+  return (
+    <>
+      {type === 'button' ? (
+        <ShadCnButton
+          className={cn(
+            'w-full bg-primary/10 cursor-pointer ring-1 ring-primary/50 backdrop-blur-3xl  hover:bg-primary/20 px-4 py-2 transition-all rounded-md font-medium',
+            className,
+          )}
+        >
+          {children}
+        </ShadCnButton>
+      ) : (
+        <Link
+          to={to}
+          className={cn(
+            `inline-block bg-primary/30 border border-accent/20 backdrop:md text-primary-foreground hover:bg-primary/40 px-4 py-2 transition-all rounded-3xl text-sm :md:text-base font-medium`,
+            className,
+          )}
+        >
+          {children}
+        </Link>
+      )}
+    </>
+  )
+}
